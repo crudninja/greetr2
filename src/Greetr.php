9b7da2d6065fca9347c775cdc0a2fda06e5a24a4
crudninja <jace@legacyseller.com>
2021-06-01 18:07:34 +0800

    initial commit

---
+++ src/Greetr.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LegacySeller\Greetr2;
+
+class Greetr
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
